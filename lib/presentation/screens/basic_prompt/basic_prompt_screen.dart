578158a1ad07507f37f45db0bb8308afbb5ba7cf
Sergio Barreras <sergiobg.isc@gmail.com>
2025-04-17 12:59:28 -0700

    Diseño del chat

--- lib/presentation/screens/basic_prompt/basic_prompt_screen.dart
+++ lib/presentation/screens/basic_prompt/basic_prompt_screen.dart
@@ -1,4 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+import 'package:uuid/uuid.dart';
+
+const user = types.User(
+    id: 'user-id-abc',
+    firstName: 'Sergio',
+    lastName: 'Barreras',
+    imageUrl: 'https://picsum.photos/id/177/200/200');
+
+const geminiUser = types.User(
+    id: 'gemini-id',
+    firstName: 'Gemini',
+    imageUrl: 'https://picsum.photos/id/179/200/200');
+
+final messages = <types.Message>[
+  types.TextMessage(author: user, id: Uuid().v4(), text: 'Hola mundo'),
+  types.TextMessage(author: geminiUser, id: Uuid().v4(), text: 'Hola Sergio'),
+];
 
 class BasicPromptScreen extends StatelessWidget {
   const BasicPromptScreen({super.key});
@@ -6,12 +25,23 @@ class BasicPromptScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Basic'),
-      ),
-      body: const Center(
-        child: Text('Basic Prompt Screen'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Promp Básico'),
+        ),
+        body: Chat(
+          messages: messages,
+          onSendPressed: (types.PartialText partialText) {
+            print(partialText.text);
+          },
+          user: user,
+          theme: const DarkChatTheme(),
+          showUserNames: true,
+          // showUserAvatars: true,
+          typingIndicatorOptions: TypingIndicatorOptions(
+              // typingUsers: [geminiUser], // TODO
+              customTypingWidget: const Center(
+                child: Text('Gemini esta pensando...'),
+              )),
+        ));
   }
 }
