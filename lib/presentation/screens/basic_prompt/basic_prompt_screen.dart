6047ff700dfe2efec12fcd9105e2f1521f93f6bb
Sergio Barreras <sergiobg.isc@gmail.com>
2025-04-17 13:41:35 -0700

    BasicChat Provider

--- lib/presentation/screens/basic_prompt/basic_prompt_screen.dart
+++ lib/presentation/screens/basic_prompt/basic_prompt_screen.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gemini_app/presentation/screens/providers/chat/basic_chat.dart';
 import 'package:gemini_app/presentation/screens/providers/chat/is_gemini_writing.dart';
 import 'package:gemini_app/presentation/screens/providers/users/user_provider.dart';
-import 'package:uuid/uuid.dart';
 
 class BasicPromptScreen extends ConsumerWidget {
   const BasicPromptScreen({super.key});
@@ -14,18 +14,17 @@ class BasicPromptScreen extends ConsumerWidget {
     final geminiUser = ref.watch(geminiUserProvider);
     final user = ref.watch(userProvider);
     final isGeminiWriting = ref.watch(isGeminiWritingProvider);
+    final chatMessages = ref.watch(basicChatProvider);
 
     return Scaffold(
         appBar: AppBar(
           title: const Text('Promp Básico'),
         ),
         body: Chat(
-          messages: [
-            types.TextMessage(author: user, id: 'Hola 2', text: 'text'),
-            types.TextMessage(author: user, id: 'Hola', text: 'text')
-          ],
+          messages: chatMessages,
           onSendPressed: (types.PartialText partialText) {
-            print(partialText.text);
+            final basicChatNotifier = ref.read(basicChatProvider.notifier);
+            basicChatNotifier.addMessage(partialText: partialText, user: user);
           },
           user: user,
           theme: const DarkChatTheme(),
---
+++ lib/presentation/screens/providers/chat/basic_chat.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_chat_types/flutter_chat_types.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:uuid/uuid.dart';
+
+part 'basic_chat.g.dart';
+
+const uuid = Uuid();
+
+@riverpod
+class BasicChat extends _$BasicChat {
+  @override
+  List<Message> build() {
+    return [];
+  }
+
+  void addMessage({
+    required PartialText partialText,
+    required User user,
+  }) {
+    _addTextMessage(partialText, user);
+  }
+
+  void _addTextMessage(PartialText partialText, User author) {
+    final message = TextMessage(
+        author: author,
+        id: uuid.v4(),
+        text: partialText.text,
+        createdAt: DateTime.now().microsecondsSinceEpoch);
+
+    state = [message, ...state];
+  }
+}
