0a736060770539e8968b870aa2f1f4aba8e41c61
Sergio Barreras <sergiobg.isc@gmail.com>
2025-04-26 01:07:26 -0700

    Variables de entorno - BaseURL

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Gemini App
 
 ## Dev
-1. Ejecutar el comando de:
+1. Clonar el arcivo `.env.template`, renombrarlo a `.env` y completar los valores necesarios.
+2. Ejecutar el comando de:
 
 ```dart
 dart run build_runner watch
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gemini_app/config/router/app_router.dart';
 import 'package:gemini_app/config/theme/app_theme.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
   AppTheme.setSystemUIOverlayStyle(isDarkMode: true);
+
+  await dotenv.load(fileName: '.env');
+
   runApp(const ProviderScope(child: GeminiApp()));
 }
 
