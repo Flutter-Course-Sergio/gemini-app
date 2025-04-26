964abce83e1f861fee9fb82076ad411f44a3397a
Sergio Barreras <sergiobg.isc@gmail.com>
2025-04-26 02:19:13 -0700

    Petición HTTP y retorno de stream

--- lib/config/gemini/gemini_impl.dart
+++ lib/config/gemini/gemini_impl.dart
@@ -17,4 +17,26 @@ class GeminiImpl {
       throw Exception("Can't get Gemini response");
     }
   }
+
+  Stream<String> getResponseStream(String message) async* {
+    try {
+      final body = jsonEncode({"prompt": message});
+      final response = await _http.post('/basic-prompt-stream',
+          data: body, options: Options(responseType: ResponseType.stream));
+
+      final stream = response.data.stream as Stream<List<int>>;
+
+      String buffer = '';
+
+      await for (final chunk in stream) {
+        final chunkString = utf8.decode(chunk, allowMalformed: true);
+        buffer += chunkString;
+
+        yield buffer;
+      }
+    } catch (e) {
+      print(e);
+      throw Exception("Can't get Gemini response");
+    }
+  }
 }
